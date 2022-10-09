505aa5eaeb91e7c7dd2189bccb0c3278775b1e26
justnotiyann <muhammadfitrian0712@gmail.com>
2022-10-09 22:11:03 +0700

    add controllers for add and delete/1.2

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,11 +1,56 @@
 const Users = require("../models/Users");
+const argon2 = require("argon2");
 
 const getUsers = async (req, res) => {
   try {
-    const result = await Users.findAll({});
+    const result = await Users.findAll({
+      attributes: ["nama", "email", "image"],
+    });
     if (result <= 0) res.json({ msg: "Data belum ada" });
-    res.json({ msg: "Berikut data Users", result });
+    res.status(200).json({ msg: "Berikut data Users", result });
   } catch (e) {
     console.log(e);
   }
 };
+
+const addUser = async (req, res) => {
+  try {
+    const hash = await argon2.hash(req.body.password);
+    const result = await Users.create({ ...req.body, password: hash });
+    if (!result) res.json({ msg: "Gagal membuat data" });
+    res.status(200).json({ msg: "berhasil membuat data" });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const updateUUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const search = await Users.findOne({ where: { id: id } });
+    if (!search) {
+      res.status(404).json({ msg: "Data user tidak ditemukan" });
+    } else {
+      const result = await Users.update(
+        { ...req.body },
+        {
+          where: { id: id },
+        }
+      );
+      if (!result) res.status(500).json({ msg: "Gagal membuat data" });
+      res.status(200).json({ msg: `Berhasil update data dengan nama ${search.nama} menjadi ${result.nama}` });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+      const search = await Users.findOne({ where: { id: id } });
+      
+  } catch (e) {}
+};
+
+module.exports = { getUsers, addUser, updateUUser };
--- routes/Users.js
+++ routes/Users.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const { ConnectionRefusedError } = require("sequelize");
+const controllers = require("../controllers/Users");
 
 router.get("/", (req, res) => {
   res.send("oke");
 });
+router.post("/add", controllers.addUser);
+router.get("/get", controllers.getUsers);
+router.post("/edit/:id", controllers.updateUUser);
 
 module.exports = router;
